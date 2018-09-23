e9c31c2a3949a3b2531451bf601899b4a8dd77f4
Sarthak Bhagat <sarthak16189@iiitd.ac.in>
2018-09-23 21:40:42 +0530

    added pd controller - mid sem

---
+++ pd_controller/armParamHW8.py
@@ -0,0 +1,24 @@
+# Single link arm Parameter File
+import numpy as np
+# import control as cnt
+import sys
+sys.path.append('..')  # add parent directory
+import armParam as P
+
+Ts = P.Ts  # sample rate of the controller
+beta = P.beta  # dirty derivative gain
+tau_max = P.tau_max  # limit on control signal
+
+#  tuning parameters
+#tr = 0.8 # part (a)
+tr = 2 # tuned to get fastest possible rise time before saturation.
+zeta = 0.707
+
+kp = 3.05
+kd = 7.27
+
+#print('kp: ', kp)
+#print('kd: ', kd)
+
+
+
